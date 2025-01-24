23be4ba45745b7da7735b63e16ccad523c29c0a9
Andrea Teixeira <andreateixeira2003@gmail.com>
2025-01-24 12:19:29 +0100

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String [] guest = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis",
+                "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Come ti chiami ?");
+        String name = s.nextLine();
+
+        boolean isPresent = false;
+        for (int i = 0; i < guest.length; i++){
+            if (name.equalsIgnoreCase(guest[i])) {
+                isPresent = true;
+                break;
+            }
+        }
+        if (isPresent){
+            System.out.println("Benvenuto");
+        }else {
+            System.out.println("NON BENVENUTO");
+        }
     }
 }
